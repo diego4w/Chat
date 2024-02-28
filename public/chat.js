031216f23d45146aa6df92e6bf47a4a0c674da2b
Diego4w <diego444x@gmail.com>
2024-02-28 19:32:26 -0300

    frontend

--- public/chat.js
+++ public/chat.js
@@ -6,28 +6,39 @@
  const form = document.getElementById('form');
  const input = document.getElementById('input');
  const messages = document.getElementById('messages');
+ const name = prompt("Qual seu username");
+ 
+ const urlParams = new URLSearchParams(window.location.search);
+ const products = urlParams.get(name) 
 
-
+ 
  // Lidar com envio de mensagem
  form.addEventListener('submit', (e) => {
      e.preventDefault();
      if (input.value) {
-         const mensage = input.value;
-         const username = 'diego'
+         const msg = input.value;
          const data = {
-             mensage,
-             username
+             msg,
+             name
          }
          socket.emit('mensagem', data);
-         console.log(username, mensage);
          input.value = '';
      }
  });
 
  // Lidar com recebimento de mensagem
  socket.on('mensagem', data => {
-     res.innerHTML += `<li>${data.username}</li>
-                       <li>${data.mensage}</li>`
-     messages.appendChild(item);
-     window.scrollTo(0, document.body.scrollHeight);
+    res.innerHTML += `
+    <div class="card bg-danger" style="margin: 10px; width: 400px; color: #fff; float: right;">
+           <div class="card-body" id="res1">
+           <div class="card-header">
+            usuario: ${data.name}
+           </div>
+             ${data.msg}
+           </div>
+         </div>`
+         
+    window.scrollTo(0, document.body.scrollHeight);
+    console.log(data.msg);
+    console.log(m);
  });
